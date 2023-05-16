9ec6587e85a945c27bd44b35f3db2d2c678d5f16
Ridah0204 <133240948+Ridah0204@users.noreply.github.com>
2023-05-16 02:37:16 -0400

    Add project files.

---
+++ Program1UsingTheObjectCout/RwejunaFloridaCout.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+	cout << "******************About me****************" << endl;
+	cout << "*Hello, My name is Florida Perfect Rwejuna. *" << endl;
+	cout << "*My major is Computer Science. *" << endl;
+	cout << "*I am freshman this year. *" << endl;
+	cout << "*I grew up in Morogoro, Tanzania. *" << endl;
+	cout << "*Fun fact about me is I can do it all. *" << endl;
+	cout << "*****************The End!************" ;
+		
+
+	return 0;
+}//end main;
